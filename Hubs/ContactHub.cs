868825e312650354b1d29a1fe0a3ec5349759d27
kyleballard <kyle.ballard80@gmail.com>
2014-06-14 11:14:32 -0500

    Initial commit

---
+++ Hubs/ContactHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalrToastr.Hubs
+{
+    public class ContactHub : Hub
+    {
+        public void ContactFormSubmitted(string name, string message)
+        {
+            // Calls the 'notifyUsers' method on the client side.
+            //  Note:  You can change 'All' to target only certain clients.
+            //  Note:  Code after 'All' is dynamic, and you can specify whatever name you wish.
+            Clients.All.notifyUsers(name);
+        }
+    }
+}
